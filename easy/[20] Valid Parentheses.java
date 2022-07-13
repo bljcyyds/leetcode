3ac5c15cf5c471e565f5ac3cf85cc42b3d837822
bljcyyds <101611789+bljcyyds@users.noreply.github.com>
2022-07-13 16:18:08 +0800

    Create [20] Valid Parentheses.java

---
+++ easy/[20] Valid Parentheses.java
@@ -0,0 +1,69 @@
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']
+//', determine if the input string is valid. 
+//
+// An input string is valid if: 
+//
+// 
+// Open brackets must be closed by the same type of brackets. 
+// Open brackets must be closed in the correct order. 
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "()"
+//Output: true
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "()[]{}"
+//Output: true
+// 
+//
+// Example 3: 
+//
+// 
+//Input: s = "(]"
+//Output: false
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length <= 10⁴ 
+// s consists of parentheses only '()[]{}'. 
+// 
+// Related Topics 栈 字符串 👍 3382 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (c == ')' && top != '(') {
+                    return false;
+                }
+                if (c == ']' && top != '[') {
+                    return false;
+                }
+                if (c == '}' && top != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
